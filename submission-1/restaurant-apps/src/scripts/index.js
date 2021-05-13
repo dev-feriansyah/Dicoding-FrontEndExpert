9d668a9730e08272211b31e9343020c48b1585bd
Feriansyah <devferiansyah@gmail.com>
2021-05-13 18:46:30 +0800

    Hero and Navbar

--- submission-1/restaurant-apps/src/scripts/index.js
+++ submission-1/restaurant-apps/src/scripts/index.js
@@ -1,4 +1,22 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const navigation = document.querySelector('#navigation');
+const navigationBtn = document.querySelector('#navigation-btn');
+const navList = document.querySelector('.nav__list a');
+
+navigationBtn.addEventListener('click', event => {
+    navigation.classList.toggle('nav-open');
+
+    let isOpen = navigation.classList.contains('nav-open');
+    let ariaExpanded = isOpen ? 'true' : 'false';
+    navigationBtn.setAttribute('aria-expanded', ariaExpanded);
+        
+    if (isOpen) {
+        setTimeout(() => {
+            navList.focus();
+        }, 500);
+    }
+
+    event.stopPropagation();
+});
