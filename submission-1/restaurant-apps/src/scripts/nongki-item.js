e546e6396ca66a0f77f7c193d0534b62a0ccc750
Feriansyah <devferiansyah@gmail.com>
2021-06-11 16:57:38 +0800

    Fix Submission 1

--- submission-1/restaurant-apps/src/scripts/nongki-item.js
+++ submission-1/restaurant-apps/src/scripts/nongki-item.js
@@ -7,18 +7,14 @@ class NongkiItem extends HTMLElement {
  
  render() {
    this.innerHTML = `
-    <img src="${this._nongki.pictureId}" alt="Gambar tempat: ${this._nongki.name}">
+    <img src="${this._nongki.pictureId}" alt="${this._nongki.name}">
     <span>Kota ${this._nongki.city}</span>
     <div>
         <h3><a href="#${this._nongki.name}">${this._nongki.name}</a></h3>
         <div class="stars" style="--rating: ${this._nongki.rating};" role="img" aria-label="Rating tempat adalah ${this._nongki.rating} dari 5"></div>
-        <p>${this.trim(this._nongki.description)}</p>
+        <p>${this._nongki.description}</p>
     </div>
   `;
- }
-
- trim(description) {
-     return description.trim().substr(0, 200) + '...';
  }
 }
  
